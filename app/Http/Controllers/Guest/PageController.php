984629a4a985f515ade91316ecab26971c2473d1
post-coder <gabrielspanu.dev@gmail.com>
2024-04-10 10:07:27 +0200

    feat: milestone completed

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     
+
     public function index() {
-        return view('home');
+
+        // prelevo tutti i film dalla tabella movies
+        $movies = Movie::all();
+
+
+        // dd($movies);
+
+
+        return view('home', compact('movies'));
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,25 @@
 
 <div class="container py-5">
     <h1>Lista dei film</h1>
+
+    
+    <div class="row row-cols-4">
+
+        {{-- @dump($movies) --}}
+
+        @foreach($movies as $movie)
+        <div class="col p-2">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie->title}}</h5>
+                    <p class="card-text">({{$movie->original_title}})</p>
+                </div>
+            </div>
+                      
+        </div>
+        @endforeach
+
+    </div>
 </div>
 
 @endsection
